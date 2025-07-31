ba9cbc23b43c5ab8730e28b3c69ddeaceed4b326
BaeJoungEun <wjddms5810@naver.com>
2025-07-31 09:56:15 +0900

    ãgeminus 함수 추가

--- calc.py
+++ calc.py
@@ -1,2 +1,3 @@
 class Calc:
-    ...
+    def getMinus(self, a, b):
+        return a - b
--- test_calc.py
+++ test_calc.py
@@ -1,2 +1,6 @@
-def test_():
-    pass
+from calc import Calc
+
+
+def test_get_minus():
+    calc = Calc()
+    assert 3 == calc.getMinus(5, 2)
