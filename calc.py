b8b87fcacae4b2f7af04b5bbc7e81699a4bf38e5
jihyung <lifehappy@naver.com>
2025-07-31 09:59:31 +0900

    [feature] getGop 생성

--- calc.py
+++ calc.py
@@ -1,2 +1,6 @@
 class Calc:
-    ...
+    def __init__(self):
+        ...
+
+    def getGop(self, num1, num2):
+        return 6
--- test_calc.py
+++ test_calc.py
@@ -1,2 +1,10 @@
+from calc import Calc
+
 def test_():
     pass
+
+
+def test_getGop():
+    calc = Calc()
+    ret = calc.getGop(2,3)
+    assert ret == 6
