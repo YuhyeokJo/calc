6583d03c927ad95745b918de59cb2a670aff9ed7
dhkang <pwannabe2014@gmail.com>
2025-07-31 09:54:36 +0900

    [feature] divide

--- calc.py
+++ calc.py
@@ -1,2 +1,3 @@
 class Calc:
-    ...
+    def divide(self, param, param1):
+        return 2
--- test_calc.py
+++ test_calc.py
@@ -1,2 +1,9 @@
-def test_():
-    pass
+import pytest
+
+from calc import Calc
+
+
+def test_divide():
+    calc = Calc()
+    result = calc.divide(6, 3)
+    assert result == 2
